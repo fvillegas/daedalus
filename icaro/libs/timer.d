ef60f285844298e5cc24ac1d5b13cb49039222e7
Francisco Villegas <fcvillegas@gmail.com>
2022-09-04 15:23:26 -0300

    clean up

--- icaro/libs/timer.d
+++
@@ -1,3 +0,0 @@
-libs/timer.o: libs/src/timer/timer.c libs/include/timer/timer.h
-
-libs/include/timer/timer.h:
--- icaro/src/imu.d
+++
@@ -1,3 +0,0 @@
-src/imu.d src/imu.o: src/imu.c libs/include/timer/timer.h
-
-libs/include/timer/timer.h:
