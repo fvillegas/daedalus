24b65d1a925b38e21219656e11c514da6e943c83
Francisco Villegas <fcvillegas@gmail.com>
2023-10-25 20:34:20 -0300

    Add documentation about configuration of timers

--- source/timer.c
+++ source/timer.c
@@ -6,23 +6,34 @@ void timer_init(void)
   // Setup timers to generate Fast PWM
   // Timer 0
   TCNT0 = 0;
-  
-  TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01);
-  TCCR0A |= (1 << WGM00);
+
+  // COM0A1 and COM0B1 connect pins to PWM to timer0  
+  TCCR0A = (1 << COM0A1) | (1 << COM0B1);
+  // Set timer0 to Fast PWM
+  TCCR0A |= (1 << WGM01) | (1 << WGM00);
+  // Set prescaler to 64
   TCCR0B = (1 << CS01) | (1 << CS00);
 
   // Timer 1
   TCNT1 = 0;
 
-  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
+  // COM1A1 and COM1B1 connect pins to PWM to timer0  
+  TCCR1A = (1 << COM1A1) | (1 << COM1B1);
+  // Set timer1 to Phase correct 8 bit
+  TCCR1A |= (1 << WGM10);
+  // Set prescaler to 64
   TCCR1B = (1 << CS11) | (1 << CS10);
 
   // Timer 2
   TCNT2 = 0;
-  
-  TCCR2A = (1 << COM2A1) | (1 << COM2B1) | (1 << WGM20);
+
+  // COM2A1 and COM2B1 connect pins to PWM to timer0  
+  TCCR2A = (1 << COM2A1) | (1 << COM2B1);
+  // Set timer1 to Phase correct 8 bit
+  TCCR2A |= (1 << WGM20);
+  // Set prescaler to 64
   TCCR2B = (1 << CS22); 
-  
+
   OCR0A = 100;
   OCR0B = 100;
   OCR1A = 100;
